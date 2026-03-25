34d4ee27e95c183ae2a4757d842640a56d9e211c
wangkevin02 <kuangwang@link.cuhk.edu.cn>
2026-03-25 16:26:44 +0000

    Deploying to gh-pages from @ wangkevin02/wangkevin02.github.io@1c2a4866a14e0867e2bee2293e7051750512189a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "wangkevin02/Resume",
+    "data-repo": "wangkevin02/wangkevin02.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
